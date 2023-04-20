8e145753398ff644a814df5be84ff3c4870fdc47
Emilio Piraino <piraino91@gmail.com>
2023-04-20 13:40:56 -0700

    added AUTH0 intergration for users.

--- src/main/java/ep/martialartstournament/martialartstournament/controller/HomeController.java
+++ src/main/java/ep/martialartstournament/martialartstournament/controller/HomeController.java
@@ -1,6 +1,11 @@
 package ep.martialartstournament.martialartstournament.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
@@ -8,8 +13,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
 
     @GetMapping("/")
-    public String index() {
-        return "home/index";
+    public String home(Model model, @AuthenticationPrincipal OidcUser principal) {
+        if (principal != null) {
+            model.addAttribute("profile", principal.getClaims());
+        }
+        return "index";
     }
-
 }
